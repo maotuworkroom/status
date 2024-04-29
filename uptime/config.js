232fc482438c4c1bfba0d9d2f76e4fadcf5008e4
LMC <37393495+maotuworkroom@users.noreply.github.com>
2024-04-29 17:58:47 +0800

    111
    11

---
+++ uptime/config.js
@@ -0,0 +1,34 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '查询小难状态（中央Service控制面板）Public Status',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'u2535041-31a7ad5059daf8eedc416a45',
+
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'GPT主站',
+      url: 'https://bing.xinu.ink/'
+    },
+    {
+      text: '晴女の天气小站',
+      url: 'https://nanxmas.github.io/Otenki'
+    },
+    {
+      text: '100％晴天女孩',
+      url: 'https://nanxmas.github.io/Otenki'
+    },
+  ],
+};
